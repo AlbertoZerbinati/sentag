3a740e95b3d4b301cef1799afdae63ee26072c9b
Alberto Zerbinati <44242851+AlbertoZerbinati@users.noreply.github.com>
2021-10-07 11:45:30 +0200

    Update axios.js: send requests to production server

--- ui/src/axios.js
+++ ui/src/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-	// baseURL: "http://3.121.162.129:8000",
-	baseURL: "http://127.0.0.1:8000",
+	baseURL: "http://3.121.162.129:8000",
+	// baseURL: "http://127.0.0.1:8000",
 	timeout: 3000,
 });
 
